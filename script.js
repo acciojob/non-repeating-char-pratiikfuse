3285b5aa7851696a7812fa72a1475bd264330ccf
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-08 11:20:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  // Write your code here
+  let map = new Map();
+  for (let i = 0; i < str.length; i++) {
+    if (map.has(str[i])) {
+      map.set(str[i], map.get(str[i]) + 1);
+    } else {
+      map.set(str[i], 1);
+    }
+  }
+
+  let keys = map.keys();
+
+  for (let character of keys) {
+    if (map.get(character) == 1) {
+      return character;
+    }
+  }
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
